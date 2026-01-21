a48d79d520a94f75bd606cf2b8e6429b1f79d6d3
SmoothIceDude <malcolmalbright@gmail.com>
2026-01-20 21:17:32 -0500

    Update CORS configuration

--- server.js
+++ server.js
@@ -8,7 +8,10 @@ require('dotenv').config();
 const app = express();
 
 // Middleware
-app.use(cors());
+app.use(cors({
+  origin: ['https://smoothicedude.github.io', 'http://localhost:3000'],
+  credentials: true
+}));
 app.use(express.json());
 
 // MongoDB Connection
